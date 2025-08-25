0c22d3d750931899fc99b34d99ac6e4bbeba6cda
york <yukiburos@gmail.com>
2025-08-25 21:42:07 +0900

    add golang lint error

--- backend/middleware/validators/user_validator.go
+++ backend/middleware/validators/user_validator.go
@@ -10,9 +10,11 @@ import (
 func ValidateEmail(email string) error {
 	// 空の場合はエラー
 	if email == "" {
+
 		return errors.New("email is required")
 	}
 	_, err := mail.ParseAddress(email)
+
 	if err != nil {
 		return fmt.Errorf("invalid email format: %w", err)
 	}
