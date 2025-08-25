ac86527092101e922ffdd9001be9cb6033f2aae9
york <yukiburos@gmail.com>
2025-08-25 21:52:12 +0900

    Revert "add golang lint error"
    This reverts commit 0c22d3d750931899fc99b34d99ac6e4bbeba6cda.

--- backend/middleware/validators/user_validator.go
+++ backend/middleware/validators/user_validator.go
@@ -10,11 +10,9 @@ import (
 func ValidateEmail(email string) error {
 	// 空の場合はエラー
 	if email == "" {
-
 		return errors.New("email is required")
 	}
 	_, err := mail.ParseAddress(email)
-
 	if err != nil {
 		return fmt.Errorf("invalid email format: %w", err)
 	}
