2423d960fa446420fb12256d3b64eee6871f1ab3
TAGUSA Kentaro <tagusa-k@grassfield.online>
2025-10-25 15:53:38 +0000

    Auth0 の導入

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,8 +1,32 @@
 'use client';
+
+import { Auth0Provider } from '@auth0/auth0-react';
 import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 const system = createSystem(defaultConfig, {});
 
 export function Provider({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider value={system}>{children}</ChakraProvider>;
+  const [redirectUri, setRedirectUri] = useState<string>();
+
+  useEffect(() => {
+    setRedirectUri(window.location.origin);
+  }, []);
+
+  if (!redirectUri) return null;
+
+  return (
+    <ChakraProvider value={system}>
+      <Auth0Provider
+        domain="dev-587pfdcuktck0mos.us.auth0.com"
+        clientId="yOAB0paScfw1XkOcLbi91LljTspELiYT"
+        cacheLocation="localstorage"
+        authorizationParams={{
+          redirect_uri: redirectUri,
+        }}
+      >
+        {children}
+      </Auth0Provider>
+    </ChakraProvider>
+  );
 }
